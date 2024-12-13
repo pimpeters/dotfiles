d90f6243261644dc59ba675afdf57c7aa0ba53ce
Pim Peters <pimpeters1995@gmail.com>
2024-12-13 15:53:57 +0100

    changed fugitive bindings

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,8 +3,8 @@ return {
     config = function()
         vim.g.gitgutter_map_keys = 0
 
-        vim.api.nvim_set_keymap('n', '<leader>Gs', ':Git<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<leader>Gd', ':Git diff<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<leader>Gb', ':Git blame<cr>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'Gs', ':Git<cr>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'Gd', ':Git diff<cr>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'Gb', ':Git blame<cr>', { noremap = true, silent = true })
     end
 }
