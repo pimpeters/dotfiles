b32a8f33894c8eed9532c344146e9128d3f5e5a3
Pim Peters <pimpeters1995@gmail.com>
2025-05-13 11:57:43 +0200

    nvim - add live preview plugin for md files

---
+++ .config/nvim/lua/plugins/live-preview.lua
@@ -0,0 +1,7 @@
+return {
+    'brianhuster/live-preview.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+        require('livepreview.config').set()
+    end
+}
