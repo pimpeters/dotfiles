8b9f5aa85c7657f1dc89a7a24ddedc157efd701b
Pim Peters <pimpeters1995@gmail.com>
2025-05-20 08:59:43 +0200

    nvim - improved lsp by adding lsp signature

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -29,6 +29,16 @@ return {
             })
         end,
     },
+    {
+        'ray-x/lsp_signature.nvim',
+        lazy = true,
+        event = 'InsertEnter',
+        config = function()
+            require('lsp_signature').setup({
+                hint_enable = false,
+            })
+        end
+    },
 
     -- Autocompletion
     {
