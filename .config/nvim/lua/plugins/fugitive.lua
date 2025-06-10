086e3429e82e5e7ad647e67093debc77ebb4961e
Pim Peters <pimpeters1995@gmail.com>
2025-06-10 19:21:44 +0200

    feat(fugitive): use <leader> prefix for git mappings
    Also changed mappings to lowercase to be more convenient

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,8 +3,8 @@ return {
     config = function()
         vim.g.gitgutter_map_keys = 0
 
-        vim.api.nvim_set_keymap('n', 'Gs', ':Git<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', 'Gd', ':Git diff<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', 'Gb', ':Git blame<cr>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<cr>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gd', ':Git diff<cr>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gb', ':Git blame<cr>', { noremap = true, silent = true })
     end
 }
