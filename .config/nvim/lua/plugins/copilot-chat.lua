07fd597f4ed3c2bd63dcd5b24c424cd10c2b963f
Pim Peters <pimpeters1995@gmail.com>
2025-06-10 19:24:33 +0200

    feat(copilot-chat): add keymappings
    Added keymappings to the copilot-chat plugin to be more accesible

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -1,11 +1,14 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {}
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "github/copilot.vim" },
+    { "nvim-lua/plenary.nvim", branch = "master" },
+  },
+  build = "make tiktoken",
+  opts = {},
+  keys = {
+    { "<leader>oo", "<cmd>CopilotChat<cr>", desc = "Open Copilot Chat" },
+    { "<leader>oc", "<cmd>CopilotChatCommit<cr>", desc = "Copilot Commit Message" },
+    { "<leader>of", "<cmd>CopilotChatFix<cr>", desc = "Copilot Fix Code" },
   },
 }
