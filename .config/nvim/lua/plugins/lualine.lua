9361b934fb0161f06cf2f3bafc4ee8d24047326c
Pim Peters <pimpeters1995@gmail.com>
2025-06-13 21:22:51 +0200

    feat(lualine): global statusline
    and also changed theme name as caps is unsupported in Linux nvim

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,8 @@ return {
     config = function()
         require('lualine').setup {
             options = {
-                theme = 'Iceberg',
+                theme = 'iceberg',
+                globalstatus = true,
             }
         }
     end
