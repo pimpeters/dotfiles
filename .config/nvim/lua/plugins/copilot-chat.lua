a33cd0b9d1c69ae173e4877cfe9281c3e2270898
Pim Peters <pimpeters1995@gmail.com>
2025-06-27 16:37:16 +0200

    feat: changed copilot bindings

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -7,8 +7,7 @@ return {
   build = "make tiktoken",
   opts = {},
   keys = {
-    { "<leader>oo", "<cmd>CopilotChat<cr>", desc = "Open Copilot Chat" },
-    { "<leader>oc", "<cmd>CopilotChatCommit<cr>", desc = "Copilot Commit Message" },
-    { "<leader>of", "<cmd>CopilotChatFix<cr>", desc = "Copilot Fix Code" },
+    { "<leader>oc", "<cmd>CopilotChat<cr>", desc = "Open Copilot Chat" },
+    { "<leader>oo", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot Optimize Code" },
   },
 }
