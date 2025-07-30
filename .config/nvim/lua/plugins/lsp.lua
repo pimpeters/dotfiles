2e97a489ecc90b736f4207b8fa83a4c6cfe68053
Pim Peters <pimpeters1995@gmail.com>
2025-07-30 13:37:32 +0200

    feat(nvim): removed custom lsp keybinds
    as nvim 0.11 has native keybinds

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -84,14 +84,6 @@ return {
 
             local lsp_attach = function(client, bufnr)
                 local opts = {buffer = bufnr}
-
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-                vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
             end
 
             lsp_zero.extend_lspconfig({
