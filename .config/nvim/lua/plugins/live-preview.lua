ea8929311b2354095662b47637fd547ad9a15c4d
Pim Peters <pimpeters1995@gmail.com>
2025-11-14 15:37:40 +0100

    chore(nvim): removed unused plugin "live preview"

--- .config/nvim/lua/plugins/live-preview.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'brianhuster/live-preview.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-        require('livepreview.config').set()
-    end
-}
